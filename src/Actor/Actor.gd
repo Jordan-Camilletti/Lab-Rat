9c877c1de1f250fef84fad215707d1d0b28d853c
Jordan <jordan.camilletti@gmail.com>
2020-03-16 19:03:55 -0500

    Player can now 'run' using shift key

--- src/Actor/Actor.gd
+++ src/Actor/Actor.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 class_name Actor
 
-export var speed: = Vector2(300,300)
+export var walkSpeed: = Vector2(300,300)
+export var runSpeed: = 1.5
 var velocity: = Vector2.ZERO
 
 func _physics_process(delta):
--- src/Actor/Player.gd
+++ src/Actor/Player.gd
@@ -1,7 +1,8 @@
 extends Actor
 
 func _physics_process(delta):
-	velocity = calculateMovement(speed, getDirection())
+	velocity = calculateMovement(getSpeed(), getDirection())
+	getHeading()
 
 func getDirection() -> Vector2:
 	return(Vector2(
@@ -9,8 +10,19 @@ func getDirection() -> Vector2:
 		Input.get_action_strength("moveDown")-Input.get_action_strength("moveUp")
 	))
 
+func getSpeed() -> Vector2:
+	if(Input.get_action_strength("run")>0):
+		return(walkSpeed*runSpeed)
+	return(walkSpeed)
+
 func calculateMovement(
 	s: Vector2,
 	d: Vector2
 ) -> Vector2:
 	return(s*d)
+
+func getHeading() -> int:
+	var projectResolution=Vector2(Globals.get("display/width"),Globals.get("display/height"))
+	var mousePos=get_viewport().get_mouse_position()
+	print(mousePos)
+	return(0)
