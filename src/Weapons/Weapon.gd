57660b71144d45fa737ba9c02cb1265a27f5bcc5
Jordan <jordan.camilletti@gmail.com>
2020-03-20 21:50:32 -0500

    Added get methods for Weapon

--- src/Weapons/Weapon.gd
+++ src/Weapons/Weapon.gd
@@ -5,9 +5,24 @@ var dist
 var damage
 var weight
 
+func _init(i,di,da,w):
+	icon=i
+	dist=di
+	damage=da
+	weight=w
+
 func _physics_process(delta):
 	#if(Input.get_action_strength("shoot")>0):
 	$sprite.texture=load("res://assets/Weapons/knife.png")
 
 func getName():
 	return(name)
+
+func getDist():
+	return(dist)
+	
+func getDamage():
+	return(damage)
+	
+func getWeight():
+	return(weight)
