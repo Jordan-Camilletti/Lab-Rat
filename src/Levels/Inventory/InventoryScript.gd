e043e4d6d08a28452cf7f89c6aceee204560b26e
Jordan <jordan.camilletti@gmail.com>
2020-04-06 13:18:53 -0500

    Added math functions to help find inventory spot

--- src/Levels/Inventory/InventoryScript.gd
+++ src/Levels/Inventory/InventoryScript.gd
@@ -21,14 +21,25 @@ func _input(event):#Inventory Input
 		get_tree().quit()
 	if(event.is_action_pressed("openInventory")):#Go back to level
 		closeInventory("res://src/Levels/LevelTest.tscn")
+	if(event.is_action_pressed("clickItem")):
+		var mousePos=get_global_mouse_position()
+		(getInventorySquare(mousePos.x,mousePos.y))
+		
+func getInventorySquare(xPos,yPos):
+	print(roundDown(xPos/115))
+	#print(yPos)
+	return(0)
 
 func closeInventory(level):#Closing back to level
 	globalVars.inventoryOpen=false
 	get_tree().change_scene(level)
-
 
 func _on_testHighlight_mouse_entered():
 	print("Area entered")
 
 func _on_testHighlight_mouse_exited():
 	print("Area exited")
+
+func roundDown(num):#Rounds down num to the nearest whole number
+	var rtn=str(num).split(".")
+	print(int(rtn[0]))
