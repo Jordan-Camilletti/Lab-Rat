0d3315bdc97b6eddf11f7eda4b23335ff0e00f44
Jordan <jordan.camilletti@gmail.com>
2020-05-26 00:02:54 -0500

    Base door now has code for swapping sprites

--- src/Items/Doors/ParentDoor.gd
+++ src/Items/Doors/ParentDoor.gd
@@ -1,5 +1,22 @@
 extends Item
 class_name ParentDoor
 
+var open=false#State of the door
+var openSprite#Sprite for the open door
+var closedSprite#Sprite for the closed door
+
 func _ready():
 	pass
+
+func getOpen():
+	return(open)
+	
+func setOpen(o):#Changing state and sprite
+	open=o
+	setSprite()
+	
+func setSprite():#Swaping the door's state
+	if(open):#Set sprite to open
+		Sprite.texture=openSprite
+	else:#Set sprite to close
+		Sprite.texture=closedSprite
--- src/Items/Doors/Test/TestDoor.gd
+++ src/Items/Doors/Test/TestDoor.gd
@@ -1,9 +1,11 @@
 extends ParentDoor
 
 func _ready():
+	openSprite=load("res://assets/Doors/Open/TestDoor.png")
+	closedSprite=load("res://assets/Doors/Closed/TestDoor.png")
 	itemName="testDoor"
 	itemID=6
 
 func _input(event):
 	if(event.is_action_pressed("doorTest")):
-		print("E")
+		setOpen(!getOpen())
