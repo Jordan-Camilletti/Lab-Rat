d37ba0cd50683dd39db9852cd6d9ad6bede4e5e7
Jordan <jordan.camilletti@gmail.com>
2020-06-21 00:22:08 -0500

    Move around door sprites to be better organized.

--- src/Obstacles/Doors/Test/TestDoor.gd
+++ src/Obstacles/Doors/Test/TestDoor.gd
@@ -2,8 +2,8 @@ extends ParentDoor
 
 func _ready():
 	key=100#Red key
-	openSprite=load("res://assets/Doors/Open/TestDoor.png")
-	closedSprite=load("res://assets/Doors/Closed/TestDoor.png")
+	openSprite=load("res://assets/Doors/Test/open.png")
+	closedSprite=load("res://assets/Doors/Test/TestDoor.png")
 	itemName="testDoor"
 	itemID=200
 	setSprite()
