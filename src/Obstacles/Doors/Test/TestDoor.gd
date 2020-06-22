30a651bc2fc771abe492101328976b7abe7940f9
Jordan <jordan.camilletti@gmail.com>
2020-06-22 17:48:41 -0500

    Added door sprites

--- src/Obstacles/Doors/Test/TestDoor.gd
+++ src/Obstacles/Doors/Test/TestDoor.gd
@@ -3,7 +3,7 @@ extends ParentDoor
 func _ready():
 	key=100#Red key
 	openSprite=load("res://assets/Doors/Test/open.png")
-	closedSprite=load("res://assets/Doors/Test/TestDoor.png")
+	closedSprite=load("res://assets/Doors/Test/closed.png")
 	itemName="testDoor"
 	itemID=200
 	setSprite()
